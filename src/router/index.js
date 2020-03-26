d12fe1a4cf4db6f7b1fe0cad0b880bbc1720a3c2
zx <917445512@qq.com>
2020-03-26 16:42:34 +0800

    second

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/login.vue'
+import Home from '../views/home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Login',
-    component: Login
+    name: 'login',
+    redirect: '/login'
   },
   
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>这是主页， 您已经登陆成功</p>
+    </div>
+</template>
